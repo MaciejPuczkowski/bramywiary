4d20251e3ed0ee9718824a09f638e06d7669c3a3
MaciejPuczkowski <puczkowski@gmail.com>
2013-04-06 16:10:59 +0200

    admin.py

---
+++ core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from core.models import NewsPage, Sentence, Day, Event, News, Photo, Gallery,\
+    Movie, About, Contact, Sponsor, HonorPatron, MediaPatron, Partner, Guest,\
+    GuestMeeting
+admin.site.register( NewsPage )     
+admin.site.register( Sentence ) 
+admin.site.register( Day )  
+admin.site.register( Event )  
+admin.site.register( News )  
+admin.site.register( Photo )  
+admin.site.register( Gallery )  
+admin.site.register( Movie )
+admin.site.register( About )    
+admin.site.register( Contact )  
+admin.site.register( Sponsor )  
+admin.site.register( HonorPatron )
+admin.site.register( MediaPatron )
+admin.site.register( Partner )      
+admin.site.register( Guest )  
+admin.site.register( GuestMeeting )  
